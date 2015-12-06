0bed8266f93fd9e7201184e13c8306774ee814b0
roulaoregan <roula.oregan@gmail.com>
2015-12-06 13:45:27 -0800

    Update classifier.py

--- library/classifier.py
+++ library/classifier.py
@@ -1,9 +1,16 @@
-#Super Abstract Classifier has the classify  method
-#all subclasses must instantiate this method variable
-#
-#
+'''
+ copyright 2014
+ author: Spiridoula O'Regan
+ email: roula.oregan@gmail.com
+ github user: roulaoregan
+'''
+
 
 class Classifier(object):
+	'''
+	Super Abstract Classifier has the classify  method
+	all subclasses must instantiate this method variable
+	'''
 	def __init__(self, input_text, languages=['english', 'french','german']):
 		self.input_text = input_text
 		self.language_ratios = {}
