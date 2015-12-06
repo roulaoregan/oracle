0607e1c2bad022b27219cd61c04a40a4be3add6f
roulaoregan <roula.oregan@gmail.com>
2015-12-06 13:58:10 -0800

    Update naivebayes.py

--- library/naivebayes.py
+++ library/naivebayes.py
@@ -4,88 +4,94 @@
  email: roula.oregan@gmail.com
  github user: roulaoregan
 '''
+# coding=utf-8
+import nltk
 import operator
 import os
 import re
 import sys
 import unicodedata
-import yaml
 
-import nltk
+from classifier import Classifier
 from nltk import classify
 from nltk import NaiveBayesClassifier
+from nltk.probability import ELEProbDist, FreqDist
 from nltk.corpus import europarl_raw, genesis, gutenberg, names, stopwords, words
 from nltk.corpus import PlaintextCorpusReader
+from nltk.corpus import stopwords
+from utilities.training import TrainingData, get_corpus
+from utilities import utils
 
-from utilities.training import TrainingData
-# freqDist of training intersection for each language with the freqDist of input text, get closest match
-#
-class OracleClassifier(object):
- 	"""docstring for OracleClassifier"""
-	def __init__(self, root_dir, input_text=""):
-		self.root_dir = root_dir
-		self.trainer = TrainingData(languages=['english','french','german'], root_dir= self.root_dir)
-		self.trainer.build_training_set()
-		self.input_text = input_text
-		self.classifier = None
+'''
+freqDist of training intersection for each language with the freqDist of input text, get closest match
+todo: 
+- add probability to classifier
+- add logger 
+
+''' 
+class OracleClassifier(Classifier):
+ 	"""OracleClassifier:  language identification classifier using NaiveBayes
+ 						  inherits from Classifier super class
+
+ 	"""
+	def __init__(self, root_dir, input_text, config_dirs):
+		Classifier.__init__(self, input_text)
+
 		self.master_word_list = []
 		self.word_features = []
-	
-	   
-	def extract_features(self, input_text):
-		document_words = set(input_text)
+		self.configs = utils.load_json_file(config_dirs)
+
+	def get_words_in(self, words):
+		all_words = []
+		for (word, language) in words:
+			all_words.append(word)
+		return all_words
+
+	def word_feats(self, wordlist):
+		wordlist = nltk.FreqDist(wordlist)
+		word_features = wordlist.keys()
+		return word_features
+
+
+	def extract(self, document):
 		features = {}
 		for word in self.word_features:
-			features['contains(%s)'%word] = (word in document_words)
+			features['contains(%s)' % word] = (word in document)
 
 		return features
 
-	def word_features(self, wordlist):
-		wordlist = nltk.FreqDist(wordlist)
-		return wordlist.keys()
-	
-	
-	def _assemble(self):
-		print "inside assemble"
-		all_words = []
-		for (language, values) in self.trainer.data.iteritems():
-			print language
-			print len(values['wordlist'])
-			all_words.append((values['wordlist'], language))
-		return all_words
 		
-	def get_words(self):
-		all_words = []
-		for (words, language) in self.master_word_list:
-			all_words.extend(words)
-		return all_words
-	
-	def get_word_features(self, wordlist):
-		print "inside get_word_features()"
-		wordlist = nltk.FreqDist(wordlist)
-		return wordlist.keys()
+	def classify(self, input_text=None):
 
-	def train(self):
-		print "inside train"
-		self.master_word_list = self._assemble()
-		word_features = self.get_word_features(self.get_words())
-		self.word_features = [re.sub(r"([^\w\.\'\-\/,&])", r'', feature) for feature in word_features]
-		self.training_set = classify.apply_features(self.extract_features, self.master_word_list)
+		training_text = []
 
-		self.classifier = NaiveBayesClassifier.train(self.training_set)
-		
-	def naive_bayes_classifier(self, input_text):
+		for language in self.languages:
+
+			corpusdir = os.path.join(self.configs['dirs']['CORPUS_DIR'], language)
+			stopwrds = stopwords.words(language)
+			training_text.extend([(w.lower(), language) for w in get_corpus(corpusdir) if len(w) >= 2 and w not in stopwrds])
+
+		self.word_features = self.word_feats(self.get_words_in(training_text))
+
+		training_set = nltk.classify.apply_features(self.extract, training_text)
+		classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+		classifier = nltk.NaiveBayesClassifier.train(training_set)
 
 		text = input_text if input_text else self.input_text
-		return self.classifier.classify(self.extract_features(text.split()))
+		prediction = classifier.classify(self.extract(self.input_text))
+		
+		return prediction
 
-
+'''
+test
+'''
 def main():
-	root_dir = "/Users/spiridoulaoregan/nltk_data"
-	genesis_text = "The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of the darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy My brothers. And you will know I am the Lord when I lay My vengeance upon you."
-	oc = OracleClassifier(root_dir,genesis_text)
-	oc.train()
-	oc.naive_bayes_classifier(genesis_text)
+	snow_queen = get_corpus(corpusdir="/Users/spiridoulaoregan/nltk_data/test_data", filename="snow_queen_german.txt")
+	#root_dir, input_text, config_dirs
+	oc = OracleClassifier(root_dir="/home/roulaoregan/algo/nltk_data", input_text=snow_queen, config_dirs=os.path.join(os.getcwd(), "configs", "dirs.json"))
+	print  "classified: ", oc.classify()
+
 
 if __name__ == "__main__":
 	sys.exit(main())
